a3fa314f75eecf37d81009fc5393a3dcbdac4837
gajapathi <gajapathi.kikkara@arthmate.com>
2025-03-16 00:49:42 +0530

    Deploy to main

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/allaboutlinux/',
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'react': path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
-    }
+    extensions: ['.js', '.jsx']
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
+      output: {
+        manualChunks: undefined
       }
     }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
   },
   css: {
     devSourcemap: true,
